8fef03cbee7ae8c448af0cd612b8d5856834866d
Chris Oakman <chris@oakmac.com>
2014-03-01 15:41:46 -0600

    slider updates the graph

--- externs/flot-0.8.2.js
+++ externs/flot-0.8.2.js
@@ -1,1 +1,3 @@
 jQuery.prototype.plot = function(arg1) {};
+jQuery.prototype.setData = function(arg1) {};
+jQuery.prototype.draw = function(arg1) {};
--- src-cljs/ap/pages/fourier.cljs
+++ src-cljs/ap/pages/fourier.cljs
@@ -7,19 +7,58 @@
     [ap.main :as main]
     [ap.util :as util]))
 
+(declare
+  create-data-series)
+
+;;------------------------------------------------------------------------------
+;; Atoms
+;;------------------------------------------------------------------------------
+
+(def x-value (atom 1))
+
+(defn on-change-x-value [_ _ _ new-x]
+  (.setData (aget js/window "fourier-chart-1") (create-data-series new-x))
+  (.draw (aget js/window "fourier-chart-1"))
+  nil
+  )
+
+(add-watch x-value :_ on-change-x-value)
+
 ;;------------------------------------------------------------------------------
 ;; Slider
 ;;------------------------------------------------------------------------------
 
+(defn on-slide [js-event js-ui]
+  (reset! x-value (.-value js-ui)))
+
 (defn init-slider []
-  (util/slider "#slider" {}))
+  (util/slider "#slider" {
+    :value @x-value
+    :min 0
+    :max 10
+    :step 1
+    :slide on-slide }))
 
 ;;------------------------------------------------------------------------------
 ;; Charts
 ;;------------------------------------------------------------------------------
 
+(defn create-data-series [x]
+  (clj->js [[[0 (* x 1)]
+    [1 (* x 1.2)]
+    [2 (* x 1.8)]
+    [3 (* x 2)]
+    [4 (* x 2.1)]
+    [5 (* x 3)]]]))
+
 (defn init-charts []
-  (util/chart "#chart1" [[[0 1] [1 2] [4 5]]] {})
+  (aset js/window "fourier-chart-1"
+    (util/chart "#chart1" (create-data-series @x-value) {
+      :yaxis {
+        :min -5
+        :max 50
+      }
+      }))
   (util/chart "#chart2" [[[0 1] [1 2] [4 5]]] {})
   (util/chart "#chart3" [[[0 1] [1 2] [4 5]]] {})
   (util/chart "#chart4" [[[0 1] [1 2] [4 5]]] {}))
